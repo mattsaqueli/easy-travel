1ceeec6ba2bdb512d68d5305c6decc79ab947b03
Matt Saqueli <msaqueli22@turing.edu>
2023-04-24 10:20:46 -0500

    Add query selectors, imports, and global variables

--- src/scripts.js
+++ src/scripts.js
@@ -9,13 +9,25 @@ import Destination from './Destination';
 
 // -------------------------------- GLOBAL -------------------------------- //
 
-let trips, travelers, destinations, currDate, date, currUser;
+let currUserID;
+let date = new Date();
+let currDate = date.getFullYear() + "/" + ("0" + (date.getMonth()+1)).slice(-2) + "/"+ ("0" + date.getDate()).slice(-2);
+let trips, travelers, destinations;
+console.log(currDate)
 
 // -------------------------------- QUERY SELECTORS -------------------------------- //
 
-
-
-
+const estimateBtn = document.querySelector('.estimate-button');
+const estimateCost = document.querySelector('.estimate-cost')
+const pastTrips = document.querySelector('.past-cards');
+const pendingTrips = document.querySelector('.pending-cards');
+const traveler = document.querySelector('.welcome-user');
+const travelTotal = document.querySelector('.cost');
+const dateInput = document.querySelector('#date');
+const durationInput = document.querySelector('#duration');
+const TravelerInput = document.querySelector('#travelers');
+const destinationInput = document.querySelector('#destination');
+const inputForm = document.querySelector('.post-form');
 
 // -------------------------------- EVENT LISTNERS -------------------------------- //
 
