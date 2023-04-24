e4d38c1c70908492e4fb4651893da257d7505872
Matt Saqueli <msaqueli22@turing.edu>
2023-04-23 23:53:20 -0500

    Add get destination method and get trip cost method

---
+++ src/Destination.js
@@ -0,0 +1,17 @@
+class Destination {
+  constructor(destinationData) {
+    this.destinationData = destinationData;
+  };
+
+  getDestination(Id) {
+    return this.destinationData.find(destination => destination.id === Id);
+  };
+
+  getCost(Id, travelers, duration) {
+    const destination = this.getDestination(Id)
+    const totalWithAgentFee = Math.round((destination.estimatedLodgingCostPerDay * duration + destination.estimatedFlightCostPerPerson * travelers) * 0.1)
+    return totalWithAgentFee;
+  };
+};
+
+export default Destination;
